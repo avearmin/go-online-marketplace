74159581fc3cf08077362b43487e18920ebba1f4
avearmin <arminradoncic1@gmail.com>
2024-01-02 23:33:19 -0500

    Refactor respondWithError to use anonymous struct

--- json.go
+++ json.go
@@ -8,12 +8,14 @@ import (
 
 func respondWithJSON(w http.ResponseWriter, status int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
+	
 	data, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
 		return
 	}
+	
 	w.WriteHeader(status)
 	w.Write(data)
 }
@@ -22,10 +24,11 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 	if code/100 == 5 {
 		log.Printf("Responding with status code %d: %s", code, msg)
 	}
-	type errorResponse struct {
+	
+	var errorResponse struct {
 		Error string `json:"error"`
 	}
-	respondWithJSON(w, code, errorResponse{
-		Error: msg,
-	})
+	errorResponse.Error = msg
+	
+	respondWithJSON(w, code, errorResponse)
 }
