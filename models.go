d76386e551bcef4c8fdeddb5dd17cf486244aa44
avearmin <arminradoncic1@gmail.com>
2024-01-04 20:45:28 -0500

    Add items table, query, and handler
    This commit adds an items table, a CreateItem query, and a handler to
    define an endpoint for the API. Currently, the POST handler is missing
    the logic to insert the seller_id into the table which requires OAuth2/token
    scheme.

--- models.go
+++ models.go
@@ -15,6 +15,17 @@ type User struct {
 	Email     string    `json:"email"`
 }
 
+type Item struct {
+	ID          uuid.UUID `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Price       int32     `json:"price"`
+	Sold        bool      `json:"sold"`
+	SellerID    uuid.UUID `json:"seller_id"`
+}
+
 func dbUserToJSONUser(user database.User) User {
 	return User{
 		ID:        user.ID,
@@ -22,5 +33,18 @@ func dbUserToJSONUser(user database.User) User {
 		UpdatedAt: user.UpdatedAt,
 		Name:      user.Name,
 		Email:     user.Email,
+	}
+}
+
+func dbItemToJSONItem(item database.Item) Item {
+	return Item{
+		ID:          item.ID,
+		CreatedAt:   item.CreatedAt,
+		UpdatedAt:   item.UpdatedAt,
+		Name:        item.Name,
+		Description: item.Description,
+		Price:       item.Price,
+		Sold:        item.Sold,
+		SellerID:    item.SellerID,
 	}
 }
