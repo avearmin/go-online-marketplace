4c7fb7b60b2d7a9d54960b35bb937540aa83bc00
avearmin <arminradoncic1@gmail.com>
2024-01-06 13:07:13 -0500

    Add ValidateJWT function to auth package

--- internal/auth/token.go
+++ internal/auth/token.go
@@ -1,26 +1,21 @@
 package auth
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 )
 
-type TokenType string
-
-func (tt TokenType) String() string {
-	return string(tt)
-}
-
 const (
-	AcessToken   TokenType = "gorage-sale-access"
-	RefreshToken TokenType = "gorage-sale-refresh"
+	AccessIssuer  string = "gorage-sale-access"
+	RefreshIssuer string = "gorage-sale-refresh"
 )
 
-func CreateJWT(id uuid.UUID, jwtSecret string, expiresIn time.Duration, tokenType TokenType) (string, error) {
+func CreateJWT(id uuid.UUID, jwtSecret string, expiresIn time.Duration, issuer string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
-		Issuer:    tokenType.String(),
+		Issuer:    issuer,
 		IssuedAt:  jwt.NewNumericDate(time.Now()),
 		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
 		Subject:   id.String(),
@@ -34,10 +29,40 @@ func CreateJWT(id uuid.UUID, jwtSecret string, expiresIn time.Duration, tokenTyp
 
 func CreateAccessToken(id uuid.UUID, jwtSecret string) (string, error) {
 	expiresIn := 1 * time.Hour
-	return CreateJWT(id, jwtSecret, expiresIn, AcessToken)
+	return CreateJWT(id, jwtSecret, expiresIn, AccessIssuer)
 }
 
 func CreateRefreshToken(id uuid.UUID, jwtSecret string) (string, error) {
 	expiresIn := (60 * 24) * time.Hour
-	return CreateJWT(id, jwtSecret, expiresIn, RefreshToken)
+	return CreateJWT(id, jwtSecret, expiresIn, RefreshIssuer)
+}
+
+func ValidateJWT(jwtString, jwtSecret string) (uuid.UUID, error) {
+	var claims jwt.RegisteredClaims
+	token, err := jwt.ParseWithClaims(jwtString, &claims, func(*jwt.Token) (interface{}, error) {
+		return []byte(jwtSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	idString, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != AccessIssuer {
+		return uuid.Nil, errors.New("invalid issuer")
+	}
+
+	id, err := uuid.Parse(idString)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return id, nil
 }
