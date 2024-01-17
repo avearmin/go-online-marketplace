118f338fc720e82aa746cec0c5adfda26340c9c4
avearmin <arminradoncic1@gmail.com>
2024-01-17 17:13:46 -0500

    Add GetHooglePublicKey function

---
+++ internal/auth/get_google_public_key.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"errors"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+
+func GetGooglePublicKey(keyID string) (string, error) {
+	resp, err := http.Get("https://www.googleapis.com/oauth2/v1/certs")
+	if err != nil {
+		return "", err
+	}
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err	
+	}
+
+	myResp := map[string]string{}
+	if err = json.Unmarshal(dat, &myResp); err != nil {
+		return "", err
+	}
+
+	key, ok := myResp[keyID]
+	if !ok {
+		return "", errors.New("key not found")
+	}
+
+	return key, nil
+}
