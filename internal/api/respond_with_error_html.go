f4ec7b352bcaee4ff264e22477631cb309ecea4d
avearmin <arminradoncic1@gmail.com>
2024-02-01 18:26:25 -0500

    Refactor respondWithErrorHTML function
    The function was refactored to provide a more general way to send
    error HTML back to the client. Previously, a simple format would be
    sent back with no flexibility for other HTML error formats. More flexibility
    allows us to accomidate more diverse HTML formats.
    
    In addition, I added respondWithHTML functions to provide a simple way
    to send html back to the client.

--- internal/api/respond_with_error_html.go
+++ internal/api/respond_with_error_html.go
@@ -1,18 +1,36 @@
 package api
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
-func respondWithErrorHTML(w http.ResponseWriter, code int, errorMsg string) {
-	errorHTML := fmt.Sprintf(`
-        <div class="error">
-            <p>%s</p>
-        </div>
-    `, errorMsg)
+func respondWithErrorHTML(w http.ResponseWriter, code int, tmplString string, errMsg string) {
+	if code/100 == 5 {
+		log.Printf("Responding with code %d: %s", code, errMsg)
+	}
 
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(code)
-	w.Write([]byte(errorHTML))
+	var payload struct {
+		Error string
+	}
+	payload.Error = errMsg
+
+	if err := respondWithHTML(w, code, tmplString, payload); err != nil {
+		log.Printf("Failed to send error: %s", err.Error())
+	}
+}
+
+func respondWithErrorHTMLFromFile(w http.ResponseWriter, code int, tmplPath string, errMsg string) {
+	if code/100 == 5 {
+		log.Printf("Responding with code %d: %s", code, errMsg)
+	}
+
+	var payload struct {
+		Error string
+	}
+	payload.Error = errMsg
+
+	if err := respondWithHTMLFromFile(w, code, tmplPath, payload); err != nil {
+		log.Printf("Failed to send error: %s", err.Error())
+	}
 }
---
+++ internal/api/respond_with_html.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func respondWithHTML(w http.ResponseWriter, code int, tmplString string, data any) error {
+	tmpl, err := template.New("template").Parse(tmplString)
+	if err != nil {
+		return err
+	}
+
+	w.WriteHeader(code)
+
+	if err := tmpl.Execute(w, data); err != nil {
+		return err
+	}
+	return nil
+}
+
+func respondWithHTMLFromFile(w http.ResponseWriter, code int, tmplPath string, data any) error {
+	tmpl, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		return err
+	}
+
+	w.WriteHeader(code)
+
+	if err := tmpl.Execute(w, data); err != nil {
+		return err
+	}
+	return nil
+}
