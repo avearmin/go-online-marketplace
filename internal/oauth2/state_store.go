084a50f5dc96a3954c3508d6ca4e2aa3fe18cc98
avearmin <arminradoncic1@gmail.com>
2024-02-06 21:15:19 -0500

    Add startReapLoop() to StateStore
    Any state generated by the server for the Google OAuth2 flow should be deleted once it's used. However, there's a possibility that a malicious actor could trigger the login endpoint without proceeding to the callback. This action would accumulate unnecessary state strings in the store that would never be deleted. To address this issue, a goroutine regularly scans the state store for expired tokens and removes them. This process ensures that all unused states are effectively managed and disposed of.

--- internal/oauth2/state_store.go
+++ internal/oauth2/state_store.go
@@ -3,17 +3,23 @@ package oauth2
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"log"
+	"sync"
 	"time"
 )
 
 type StateStore struct {
 	store map[string]time.Time
+	mu    *sync.Mutex
 }
 
 func NewStateStore() StateStore {
-	return StateStore{
+	stateStore := StateStore{
 		store: make(map[string]time.Time),
+		mu:    &sync.Mutex{},
 	}
+	stateStore.startReapLoop()
+	return stateStore
 }
 
 func (s StateStore) GenerateState() (string, error) {
@@ -22,7 +28,9 @@ func (s StateStore) GenerateState() (string, error) {
 		return "", err
 	}
 	state := base64.URLEncoding.EncodeToString(b)
+	s.mu.Lock()
 	s.store[state] = time.Now().Add(5 * time.Minute)
+	s.mu.Unlock()
 	return state, nil
 }
 
@@ -38,5 +46,29 @@ func (s StateStore) ValidateState(state string) bool {
 }
 
 func (s StateStore) DeleteState(state string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	delete(s.store, state)
+}
+
+// the Google callback handler should delete states from the server once they're used,
+// but we need to reap any expired states that might have accumulated for whatever reason
+func (s StateStore) startReapLoop() {
+	go func() {
+		for {
+			time.Sleep(5 * time.Minute)
+			log.Println("awakening state reaper")
+			s.reap()
+			log.Println("state reaper going back to sleep")
+		}
+	}()
+}
+
+func (s StateStore) reap() {
+	for k, v := range s.store {
+		if time.Now().After(v) {
+			log.Printf("Reaping: %s", k)
+			s.DeleteState(k)
+		}
+	}
 }
